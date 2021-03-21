9d63c2efe0c5d05699471603cca7ef8f54dd1840
d00kIe <dimitarkurtev@gmail.com>
2021-03-21 12:06:25 +0100

    horizontal calendar, today styling

--- app/src/main/java/com/teraculus/lingojournalandroid/ui/stats/StatsScreen.kt
+++ app/src/main/java/com/teraculus/lingojournalandroid/ui/stats/StatsScreen.kt
@@ -3,14 +3,18 @@ package com.teraculus.lingojournalandroid.ui.stats
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.teraculus.lingojournalandroid.ui.calendar.Calendar
 
 @Composable
 fun StatsScreen() {
     Column(Modifier.fillMaxHeight()) {
 
-        Calendar(Modifier.fillMaxWidth())
+        Calendar(Modifier.fillMaxWidth(), onClick = {})
+        Divider()
     }
 }
