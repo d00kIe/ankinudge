156938d2c54c873bb2f82581e8d67d45ff68820a
d00kIe <dimitarkurtev@gmail.com>
2021-04-05 08:50:42 +0200

    First attempt on background svg

--- app/src/main/java/com/teraculus/lingojournalandroid/ui/home/HomeScreen.kt
+++ app/src/main/java/com/teraculus/lingojournalandroid/ui/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.teraculus.lingojournalandroid.ui.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -9,8 +10,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.teraculus.lingojournalandroid.R
 import com.teraculus.lingojournalandroid.ui.components.ActivityRow
 import com.teraculus.lingojournalandroid.utils.toDayString
 
@@ -35,6 +38,11 @@ fun ActivityList(
     LazyColumn(
     ) {
         item {
+//            if(MaterialTheme.colors.isLight) {
+//                Image(painter = painterResource(id = R.drawable.lightbackground), contentDescription = null)
+//            } else {
+//                Image(painter = painterResource(id = R.drawable.darkbackground), contentDescription = null)
+//            }
             Text("Journal", modifier = Modifier.padding(horizontal = 16.dp, vertical = 32.dp), style = MaterialTheme.typography.h4)
         }
         if (groups != null && groups.orEmpty().isNotEmpty()) {
