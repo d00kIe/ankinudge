2317bb9bb863e5428021fece0aaf9c1b82bcad15
d00kIe <dimitarkurtev@gmail.com>
2021-04-05 16:35:57 +0200

    truncate text

--- app/src/main/java/com/teraculus/lingojournalandroid/ui/components/ActivityRow.kt
+++ app/src/main/java/com/teraculus/lingojournalandroid/ui/components/ActivityRow.kt
@@ -1,18 +1,17 @@
 package com.teraculus.lingojournalandroid.ui.components
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.teraculus.lingojournalandroid.data.getLanguageDisplayName
@@ -35,21 +34,15 @@ fun ActivityRow(activity: Activity, onClick: (id: String) -> Unit) {
     val language by model.language.observeAsState()
     val confidence by model.confidence.observeAsState()
     val motivation by model.motivation.observeAsState()
-    Card(elevation = 0.dp,
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable(onClick = { onClick(activity.id.toString()) })) {
-        Column(modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 8.dp)) {
-            ListItem(
-                icon = { ActivityRowIcon(category?.icon, confidence, motivation, category?.color) },
-                text = { Text(title.orEmpty()) },
-                secondaryText = { if(text.orEmpty().isNotEmpty()) Text(text.orEmpty()) },
-                overlineText = { OverlineText(startTime, language, type?.name) })
-        }
-    }
+    ListItem(
+        modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp).clickable(onClick = { onClick(activity.id.toString()) }),
+        icon = { ActivityRowIcon(category?.icon, confidence, motivation, category?.color) },
+        text = { Text(title.orEmpty()) },
+        secondaryText = { if(text.orEmpty().isNotEmpty()) Text(text.orEmpty(), maxLines = 3, overflow = TextOverflow.Ellipsis) },
+        overlineText = { OverlineText(startTime, language, type?.name) })
+
 }
+
 @Composable
 fun OverlineText(startTime: LocalTime?, language: String?, typeName: String?) {
     val text = "${toTimeString(startTime)} · ${getLanguageDisplayName(language.orEmpty())} · ${ typeName.orEmpty() }"
