6711a9e5d0a71a103ec068a53a3a6d767b87826f
d00kIe <dimitarkurtev@gmail.com>
2021-07-13 23:09:45 +0200

    Faster start up, move admob initialization in another thread

--- app/src/main/java/com/teraculus/lingojournalandroid/App.java
+++ app/src/main/java/com/teraculus/lingojournalandroid/App.java
@@ -1,6 +1,7 @@
 package com.teraculus.lingojournalandroid;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.RequestConfiguration;
@@ -34,16 +35,21 @@ public class App extends Application {
         // Initialize billing
         BillingManager.Companion.init(this);
 
-        // Initialize the Mobile Ads SDK.
-        MobileAds.initialize(this, initializationStatus -> {
-            List<String> testDeviceIds = Arrays.asList("C3B9A2FC8DCF3291516F3FB843E4A000");
-            RequestConfiguration configuration =
-                    new RequestConfiguration.Builder().setTestDeviceIds(testDeviceIds).build();
-            MobileAds.setRequestConfiguration(configuration);
-
-            MobileAds.setAppMuted(true);
-        });
-
+        Context context = this;
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                // Initialize the Mobile Ads SDK.
+                MobileAds.initialize(context, initializationStatus -> {
+                    List<String> testDeviceIds = Arrays.asList("C3B9A2FC8DCF3291516F3FB843E4A000");
+                    RequestConfiguration configuration =
+                            new RequestConfiguration.Builder().setTestDeviceIds(testDeviceIds).build();
+                    MobileAds.setRequestConfiguration(configuration);
+                    MobileAds.setAppMuted(true);
+                });
+            }
+        };
+        thread.start();
     }
 
 }
